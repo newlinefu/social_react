6109af9ec08985352ea72665ac25560ba8660aab
Alexandr Smirnov <newlinefu@gmail.com>
2020-06-23 21:48:29 +0300

    Updates

---
+++ precache-manifest.2daa078004a33bc992a49f4e49ec9131.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "424200b476d49f8c44e73fa97218d2f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb7927c771cf3d7ecf0e",
+    "url": "/static/css/main.fcd5a891.chunk.css"
+  },
+  {
+    "revision": "d0d7f62238519273ff06",
+    "url": "/static/js/2.f9649518.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f9649518.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb7927c771cf3d7ecf0e",
+    "url": "/static/js/main.2ead18f3.chunk.js"
+  },
+  {
+    "revision": "f0040732e9af1cac02a5",
+    "url": "/static/js/runtime-main.86a5ab79.js"
+  },
+  {
+    "revision": "578242859028cb5cd8fa5cab79b01a37",
+    "url": "/static/media/headerLogoImage.57824285.jpg"
+  }
+]);
